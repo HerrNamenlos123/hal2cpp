34a4c0b12447c06862199be8632b412cdac37b15
HerrNamenlos123 <herrnamenlos123@gmail.com>
2025-04-06 17:52:31 +0200

    add docker sim

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,23 @@ RUN apt update && apt install -y \
   x11-apps \
   && apt clean
 
-CMD ["linuxcnc"]
+RUN apt install -y build-essential cmake
+RUN apt install -y git
+RUN apt install -y linuxcnc-uspace-dev
+
+WORKDIR /app
+RUN mkdir -p build
+
+COPY ./configs ./configs
+COPY ./CMakeLists.txt ./CMakeLists.txt
+COPY ./generate.py ./generate.py
+COPY ./config.json ./config.json
+
+RUN cmake -S . -B build -DONLY_FETCH_SOCKPP=TRUE
+
+COPY ./src ./src
+RUN cmake -S . -B build -DHAL2CPP_MAIN_CONFIG_HAL_FILE="/app/configs/hal2cpp-config/hal2cpp-config.hal" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+
+RUN cmake --build build --target install
+
+CMD $START_CMD $CONFIG_FILE
--- src/hal2cpp.cpp
+++ src/hal2cpp.cpp
@@ -1,34 +1,32 @@
 
-#include "hal2cpp.h"
-#include <cmath>
+#include "hal/hal2cpp.h"
 #include <chrono>
+#include <cmath>
 #include <iostream>
 #include <ostream>
 
-uint64_t micros() {
-    static auto start = std::chrono::high_resolution_clock::now();
-    auto now = std::chrono::high_resolution_clock::now();
-    return std::chrono::duration_cast<std::chrono::microseconds>(now - start).count();
+uint64_t micros()
+{
+  static auto start = std::chrono::high_resolution_clock::now();
+  auto now = std::chrono::high_resolution_clock::now();
+  return std::chrono::duration_cast<std::chrono::microseconds>(now - start).count();
 }
 
-extern "C" int hal2cpp_setup() {
-    return 0;
+extern "C" int hal2cpp_setup() { return 0; }
+
+extern "C" void hal2cpp_loop(struct Data* data)
+{
+  data->out.act_pos.x = data->in.cmd_pos.x;
+  data->out.act_pos.y = data->in.cmd_pos.y;
+  data->out.act_pos.z = data->in.cmd_pos.z;
+  data->out.hardware_estop = data->in.virtual_estop;
+
+  auto start = micros();
+  auto d = data;
+  auto s = sizeof(Data);
+  auto elapsed = micros() - start;
+
+  // data->out.act_pos.x = 187;
 }
 
-extern "C" void hal2cpp_loop(struct Data* data) {
-    data->out.act_pos.x = data->in.cmd_pos.x;
-    data->out.act_pos.y = data->in.cmd_pos.y;
-    data->out.act_pos.z = data->in.cmd_pos.z;
-    data->out.hardware_estop = data->in.virtual_estop;
-
-    auto start = micros();
-    auto d = data;
-    auto s = sizeof(Data);
-    auto elapsed = micros() - start;
-
-    //data->out.act_pos.x = 187;
-}
-
-extern "C" void hal2cpp_exit() {
-        
-}
+extern "C" void hal2cpp_exit() { }
