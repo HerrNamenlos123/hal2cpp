ec5c46fb56e4e566cf5400f3d01a59c08dad7931
HerrNamenlos123 <herrnamenlos123@gmail.com>
2025-04-06 18:58:45 +0200

    python generator

--- src/hal2cpp.cpp
+++ src/hal2cpp.cpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <cmath>
 #include <iostream>
+#include <memory.h>
 #include <ostream>
 
 uint64_t micros()
@@ -12,10 +13,23 @@ uint64_t micros()
   return std::chrono::duration_cast<std::chrono::microseconds>(now - start).count();
 }
 
-extern "C" int hal2cpp_setup() { return 0; }
+const size_t bufferlen = 16;
+Vec5 buffer[bufferlen];
+
+uint64_t startTime = 0;
+
+extern "C" int hal2cpp_setup()
+{
+  // memset(buffer, 0, sizeof(buffer));
+  startTime = micros();
+  return 0;
+}
 
 extern "C" void hal2cpp_loop(struct Data* data)
 {
+  uint64_t now = micros();
+  double elapsed = (now - startTime) / 1000000.0;
+
   data->out.act_pos.x = data->in.cmd_pos.x;
   data->out.act_pos.y = data->in.cmd_pos.y;
   data->out.act_pos.z = data->in.cmd_pos.z;
@@ -24,9 +38,14 @@ extern "C" void hal2cpp_loop(struct Data* data)
   auto start = micros();
   auto d = data;
   auto s = sizeof(Data);
-  auto elapsed = micros() - start;
+  // auto elapsed = micros() - start;
 
-  // data->out.act_pos.x = 187;
+  // Vec5 newPos = buffer[0];
+  // for (int i = 0; i < bufferlen - 1; i++) {
+  //   buffer[i] = buffer[i + 1];
+  // }
+
+  // data->out.act_pos.x = 20 * sin(elapsed);
 }
 
 extern "C" void hal2cpp_exit() { }
